8682d8948fb301fc59a05d7fb770ea70eb457328
Nidhish Soni <nidhishsoni08@gmail.com>
2026-05-15 14:14:52 +0530

    Add files via upload

--- database/portfolio.sql
+++ database/portfolio.sql
@@ -1,1 +1,14 @@
+CREATE DATABASE portfolio;
 
+USE portfolio;
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(100),
+    technology VARCHAR(100)
+);
+
+INSERT INTO projects(project_name, technology)
+VALUES
+('Portfolio Website', 'HTML,CSS,Node.js'),
+('Calculator App', 'JavaScript');
